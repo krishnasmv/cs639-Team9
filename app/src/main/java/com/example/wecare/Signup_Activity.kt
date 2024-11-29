23b0fa370024292877934a0f48f4b01433a4b7b5
Mohan <krishna@savatas-MacBook-Air.local>
2024-11-29 16:19:20 -0500

    Add sign up screen

---
+++ app/src/main/java/com/example/wecare/Signup_Activity.kt
@@ -0,0 +1,15 @@
+package com.example.wecare
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Signup_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+    }
+}
